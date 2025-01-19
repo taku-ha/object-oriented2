c97e74106ff4db6abd017c67cc2d5d60ebfa2618
hatoritakuma <takuma.hatori@rakus-partners.co.jp>
2025-01-20 08:53:30 +0900

    third commit

---
+++ src/practice/Ex01.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        //System.out.println("羽鳥拓磨");
+
+        
+    }
+}
---
+++ src/practice/Ex05.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        String name;
+        name = "羽鳥拓磨";
+        System.out.println(name);
+
+        int number;
+        
+
+    }
+}
